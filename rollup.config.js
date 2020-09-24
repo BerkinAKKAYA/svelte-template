a6e28d740c3c648c6b859ea61b74731442e4efb4
Berkin AKKAYA <berkin_akkaya@hotmail.com>
2020-09-24 16:57:04 +0300

    Name Removed & Prettified

--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,34 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
-import serve from 'rollup-plugin-serve';
-import svelte from 'rollup-plugin-svelte';
-import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
-import sveltePreprocess from 'svelte-preprocess';
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import serve from "rollup-plugin-serve";
+import svelte from "rollup-plugin-svelte";
+import livereload from "rollup-plugin-livereload";
+import { terser } from "rollup-plugin-terser";
+import sveltePreprocess from "svelte-preprocess";
 
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-	input: 'main.ts',
+	input: "main.ts",
 	output: {
 		sourcemap: !production,
-		format: 'iife',
-		name: 'app',
-		file: 'public/build/bundle.js'
+		format: "iife",
+		file: "public/build/bundle.js",
 	},
 	plugins: [
 		svelte({
 			dev: !production,
-			css: css => css.write('bundle.css', !production),
+			css: (css) => css.write("bundle.css", !production),
 			preprocess: sveltePreprocess(),
 		}),
-		resolve({ browser: true, dedupe: ['svelte'] }),
+		resolve({ browser: true, dedupe: ["svelte"] }),
 		commonjs(),
 		typescript({ sourceMap: !production, inlineSources: !production }),
 
-		!production && serve({ contentBase: './public', port: 5000 }),
-		!production && livereload('public'),
-		production && terser()
+		!production && serve({ contentBase: "./public", port: 5000 }),
+		!production && livereload("public"),
+		production && terser(),
 	],
-	watch: { clearScreen: true }
+	watch: { clearScreen: true },
 };
